703d254f757ac7e1d19ebc7c2c7ccae530eedd7e
phuclc17 <phucle172005@gmail.com>
2025-12-19 05:59:20 +0700

    phuc

--- app/src/main/java/com/example/proj_ecom_mobile/model/CartItem.java
+++ app/src/main/java/com/example/proj_ecom_mobile/model/CartItem.java
@@ -1,4 +1,62 @@
 package com.example.proj_ecom_mobile.model;
 
-public class CartItem {
+import java.io.Serializable;
+
+public class CartItem implements Serializable {
+    private String productId;
+    private String productName;
+    private double productPrice;
+    private String productImage;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(String productId, String productName, double productPrice, String productImage, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productImage = productImage;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- app/src/main/java/com/example/proj_ecom_mobile/model/User.java
+++ app/src/main/java/com/example/proj_ecom_mobile/model/User.java
@@ -1,4 +1,52 @@
 package com.example.proj_ecom_mobile.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String uid;
+    private String email;
+    private String name;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String uid, String email, String name, String role) {
+        this.uid = uid;
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
